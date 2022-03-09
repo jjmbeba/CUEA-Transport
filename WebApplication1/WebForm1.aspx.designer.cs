7b03a590a4c14669341ebbbe823e90dfe55565f9
Johnson Mbeba <jjmbeba@gmail.com>
2022-03-09 14:20:08 +0300

    New file uploaded

--- WebApplication1/WebForm1.aspx.designer.cs
+++ WebApplication1/WebForm1.aspx.designer.cs
@@ -11,16 +11,25 @@ namespace WebApplication1
 {
 
 
-    public partial class SiteMaster
+    public partial class WebForm1
     {
 
         /// <summary>
-        /// MainContent control.
+        /// form1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder MainContent;
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Label1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
     }
 }
